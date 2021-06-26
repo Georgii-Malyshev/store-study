8783c28ebf67442bf1dbc77163d3d4c40c2a6ee4
Georgii-Malyshev <terminal451@gmail.com>
2021-06-26 11:35:23 +0500

    app now closes entityManagerFactory when stopped (i.e on contextDestroyed event)

--- StoreStudy/src/main/java/ui/AppListener.java
+++ StoreStudy/src/main/java/ui/AppListener.java
@@ -12,22 +12,24 @@ import javax.servlet.annotation.WebListener;
 
 @WebListener
 public class AppListener implements ServletContextListener {
+	
+	EntityManagerFactory entityManagerFactory;
+	ProductCatalog productCatalog;
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		ServletContext servletContext = event.getServletContext();
+		// create EntityManagerFactory instance to be used application-wide
+		entityManagerFactory = Persistence.createEntityManagerFactory("HibernateJPA");
+		servletContext.setAttribute("entityManagerFactory", entityManagerFactory);
 		// create ProductCatalog instance to be used application-wide
 		// must modify this code so that it populates productCatalog with data from
 		// persistent storage
-		ProductCatalog productCatalog = new ProductCatalog();
+		productCatalog = new ProductCatalog();
 		servletContext.setAttribute("productCatalog", productCatalog);
-		// create EntityManagerFactory instance to be used application-wide
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("HibernateJPA");
-		servletContext.setAttribute("entityManagerFactory", entityManagerFactory);
 	}
-
+	@Override
 	public void contextDestroyed(ServletContextEvent event) {
-
-		// must close entityManagerFactory when application stops
+		entityManagerFactory.close();
 	}
 }
