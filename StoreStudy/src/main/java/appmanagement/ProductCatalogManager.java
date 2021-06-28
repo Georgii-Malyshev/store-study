5d66fc5db833548e800308119e9e14b36fa7668e
Georgii-Malyshev <terminal451@gmail.com>
2021-06-28 21:25:08 +0500

    ProductCatalogManager now sets productCatalog object (after it's ready) as an attribute named "product_catalog" into the ServletContext (needs testing)

--- StoreStudy/src/main/java/appmanagement/ProductCatalogManager.java
+++ StoreStudy/src/main/java/appmanagement/ProductCatalogManager.java
@@ -1,17 +1,23 @@
 package appmanagement;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
 import domain.store.ProductCatalog;
 
-public class ProductCatalogManager implements EntityManagerFactoryListener {
-
+public class ProductCatalogManager implements ServletContextListener, EntityManagerFactoryListener {
+	private static ServletContext servletContext;
 	private static ProductCatalog productCatalog; 
+	
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		servletContext = event.getServletContext();
+	}
 	
 	@Override
 	public void entityManagerFactoryCreated() {
 		productCatalog = new ProductCatalog();
-	}
-	
-	public ProductCatalog getProductCatalog() {
-		return productCatalog;
+		servletContext.setAttribute("product_catalog", productCatalog);
 	}
 }
--- StoreStudy/src/main/java/ui/ProductCatalogServlet.java
+++ StoreStudy/src/main/java/ui/ProductCatalogServlet.java
@@ -6,13 +6,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @WebServlet("/catalog")
 public class ProductCatalogServlet extends HttpServlet {
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	RequestDispatcher requestDispatcher = request.getRequestDispatcher("/jsp/catalog.jsp");
-	requestDispatcher.forward(request, response);
-    }
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/jsp/catalog.jsp");
+		requestDispatcher.forward(request, response);
+	}
 }
