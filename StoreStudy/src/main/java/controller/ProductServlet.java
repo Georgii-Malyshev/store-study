6dbc6acc8290bf437dd3d1cf5945fb560982af25
Georgii-Malyshev <terminal451@gmail.com>
2021-07-03 12:28:47 +0500

    product page now shows product (which is selected by passing wanted product's ID as a URL query string parameter from category page)

--- StoreStudy/src/main/java/controller/ProductServlet.java
+++ StoreStudy/src/main/java/controller/ProductServlet.java
@@ -1,16 +1,39 @@
 package controller;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.ProductCategoryDao;
+import dao.ProductDao;
+import service.appmanagement.AppContextManager;
+import service.domain.store.Product;
+import service.domain.store.ProductCategory;
+
 import java.io.IOException;
 
 @WebServlet("/product") // should decide on the mapping later
 public class ProductServlet extends HttpServlet {
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	// get product in some way to show it on front end
-    }
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int productId = Integer.parseInt(request.getParameter("product_id"));
+
+		// TODO must change this code so that UI layer doesn't handle
+		// persistence-related stuff such as EntityManager and dosen't communicate to
+		// DAO layer directly
+		EntityManagerFactory entityManagerFactory = AppContextManager.getEntityManagerFactory();
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		ProductDao productDao = new ProductDao(entityManager);
+		Product product = productDao.findById(productId);
+		entityManager.close();
+
+		request.setAttribute("product", product);
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/jsp/product.jsp");
+		requestDispatcher.forward(request, response);
+	}
 }
--- StoreStudy/src/main/webapp/jsp/product.jsp
+++ StoreStudy/src/main/webapp/jsp/product.jsp
@@ -1,18 +1,17 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java"%>
 <%@ page isELIgnored="false"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
 <html>
 <head>
-<title>Product category page</title>
+<meta charset="UTF-8">
+<title>Product page</title>
 </head>
 <body>
 	<div>
 		<ul>
-			<c:forEach var="product"
-				items="${requestScope.productCategory.products}">
-				<li><a href="product?product_id=${product.id}">${product.name}
-				</a></li>
-			</c:forEach>
+			<li>name: ${requestScope.product.name} </li>
+			<li>price: ${requestScope.product.price} </li>
 		</ul>
 	</div>
 </body>
