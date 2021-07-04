3e02c1d816fc0ff7a0811f11843d90e9fe69f4f4
Georgii-Malyshev <terminal451@gmail.com>
2021-07-04 11:17:04 +0500

    added dependency: hamcrest-all v1.3 added unit test for Product class' equals()

---
+++ StoreStudy/src/test/java/service/domain/store/ProductTest.java
@@ -0,0 +1,30 @@
+package service.domain.store;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ProductTest {
+	private Product product1;
+	private Product product2;
+	private Product product3;
+	private Product product4;
+	private Product product5;
+	
+	@Before
+	public void setUp() {
+		System.out.println("initializing product objects for the test");
+		product1 = new Product("name1", 10);
+		product2 = new Product("name1", 10);
+		product3 = new Product("name3", 10);
+		product4 = new Product("name1", 40);
+		product5 = new Product("name5", 50);
+	}
+	
+	@Test
+	public void whenEqualsEqualObjectsThenReturnTrue () {
+		assertThat((product1.equals(product2)), is(true));
+	}
+}
