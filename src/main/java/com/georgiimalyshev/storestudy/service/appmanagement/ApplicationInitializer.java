8faa6af693f18ff34b3380fc2d039ffcaa7e1a33
Georgii-Malyshev <terminal451@gmail.com>
2021-07-11 21:12:08 +0500

    Move ProductCatalog retrieval from ApplicationInitializer to ProductCatalogServlet

--- src/main/java/com/georgiimalyshev/storestudy/service/appmanagement/ApplicationInitializer.java
+++ src/main/java/com/georgiimalyshev/storestudy/service/appmanagement/ApplicationInitializer.java
@@ -1,19 +1,14 @@
 package com.georgiimalyshev.storestudy.service.appmanagement;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.georgiimalyshev.storestudy.SpringApplicationContextConfig;
-import com.georgiimalyshev.storestudy.service.domain.store.ProductCatalog;
 
 @WebListener
 public final class ApplicationInitializer implements ServletContextListener {
@@ -25,12 +20,8 @@ public final class ApplicationInitializer implements ServletContextListener {
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringApplicationContextConfig.class);
 		servletContext = event.getServletContext();
 		servletContext.setAttribute("applicationContext", applicationContext);
-		
 		// TODO get rid of hardcoded catalog ID?
-		// TODO move productCatalog-related code to ProductCatalogServlet
-		int id = 1;
-		ProductCatalogService productCatalogService = applicationContext.getBean(ProductCatalogService.class);
-		ProductCatalog productCatalog = productCatalogService.getProductCatalogByIdAndFetchCategories(id);
-		servletContext.setAttribute("productCatalog", productCatalog);
+		int catalogId = 1;
+		servletContext.setAttribute("catalog_id", catalogId);
 	}
 }
