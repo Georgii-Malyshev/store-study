485132f5579f912c27bab00bb79ad363796d7305
Georgii-Malyshev <terminal451@gmail.com>
2021-07-13 23:21:04 +0500

    Delete unnecessary servlet LoginAuthServlet and move it's functions to LoginServlet

--- src/main/java/com/georgiimalyshev/storestudy/controller/LoginServlet.java
+++ src/main/java/com/georgiimalyshev/storestudy/controller/LoginServlet.java
@@ -1,11 +1,19 @@
 package com.georgiimalyshev.storestudy.controller;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.ApplicationContext;
+
+import com.georgiimalyshev.storestudy.service.appmanagement.AuthService;
+import com.georgiimalyshev.storestudy.service.domain.users.AppUser;
+
 import java.io.IOException;
 
 @WebServlet("/login")
@@ -15,5 +23,26 @@ public class LoginServlet extends HttpServlet {
 			throws ServletException, IOException {
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/jsp/login.jsp");
 		requestDispatcher.forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		ServletContext servletContext = request.getServletContext();
+		ApplicationContext applicationContext = (ApplicationContext) servletContext.getAttribute("applicationContext");
+		AuthService authService = applicationContext.getBean(AuthService.class);
+
+		AppUser appUser = authService.getAppUserByCredentials(email, password);
+
+		if (appUser.getId() != -1) {
+			HttpSession session = request.getSession();
+			session.setAttribute("appUser", appUser); // TODO maybe try something less insecure later
+			response.sendRedirect(request.getContextPath() + "/jsp/home.jsp");
+		} else {
+			response.sendError(401);
+		}
 	}
 }
