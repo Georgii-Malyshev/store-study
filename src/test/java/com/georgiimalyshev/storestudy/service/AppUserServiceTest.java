0439e205c9f6e71461631e7b8b810c0c42fde705
Georgii-Malyshev <terminal451@gmail.com>
2021-07-22 07:54:52 +0500

    Add test to AppUserService unit tests and fix an error
    Object 'customer1' of type AppUser get correctly declared and instantiated now

--- src/test/java/com/georgiimalyshev/storestudy/service/AppUserServiceTest.java
+++ src/test/java/com/georgiimalyshev/storestudy/service/AppUserServiceTest.java
@@ -2,10 +2,13 @@ package com.georgiimalyshev.storestudy.service;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,30 +23,47 @@ import com.georgiimalyshev.storestudy.service.domain.users.Customer;
 public class AppUserServiceTest {
 	private String email;
 	private String password;
+	private AppUser customer1 = new Customer();
 
 	@Mock
 	private AppUserDao appUserDao;
 
 	@InjectMocks
 	private AppUserService appUserService;
-	
+
+	// TODO write test cases for other classes implementing AppUser as well
+
+	@BeforeEach
+	public void customerSetUp() {
+		email = "email1@mail.com";
+		password = "password1";
+		customer1.setId(1);
+		customer1.setEmail(email);
+		customer1.setPassword(password);
+	}
+
 	@Test
 	public void givenCorrectCredentials_WhenFindAppUserByCredentials_ThenReturnOptionalOfCorrespondingAppUser() {
 		email = "email1@mail.com";
 		password = "password1";
-		// TODO also check with other classes implementing AppUser
-		AppUser customer1 = new Customer();
-		customer1.setId(1);
-		customer1.setEmail(email);
-		customer1.setPassword(password);
 
-		when(appUserDao.findByCredentials(email, password)).thenReturn(Optional.of(customer1));
-
+		Optional<AppUser> optionalOfCustomer1 = Optional.of(customer1);
+		when(appUserDao.findByCredentials(email, password)).thenReturn(optionalOfCustomer1);
 		Optional<AppUser> optionalOfAppUser = appUserService.findAppUserByCredentials(email, password);
-		assertAll(
-				() -> assertEquals(1, optionalOfAppUser.get().getId()),
+
+		assertAll(() -> assertEquals(1, optionalOfAppUser.get().getId()),
 				() -> assertEquals("email1@mail.com", optionalOfAppUser.get().getEmail()),
-				() -> assertEquals("password1", optionalOfAppUser.get().getPassword())
-			);
+				() -> assertEquals("password1", optionalOfAppUser.get().getPassword()));
+	}
+
+	@Test
+	public void givenEmptyCredentials_WhenFindAppUserByCredentials_ThenReturnEmptyOptional() {
+		email = "";
+		password = "";
+
+		when(appUserDao.findByCredentials(email, password)).thenReturn(Optional.empty());
+		Optional<AppUser> optionalOfAppUser = appUserService.findAppUserByCredentials(email, password);
+
+		assertTrue(optionalOfAppUser.isEmpty());
 	}
 }
