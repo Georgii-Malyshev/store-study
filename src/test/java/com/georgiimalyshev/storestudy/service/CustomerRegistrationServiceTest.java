b1ea9d3ffe10ce5061413b5266b7f19879a85ad0
Georgii-Malyshev <terminal451@gmail.com>
2021-07-25 13:38:23 +0500

    Create unit tests for CustomerRegistrationService

---
+++ src/test/java/com/georgiimalyshev/storestudy/service/CustomerRegistrationServiceTest.java
@@ -0,0 +1,74 @@
+package com.georgiimalyshev.storestudy.service;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.georgiimalyshev.storestudy.dao.CustomerDao;
+import com.georgiimalyshev.storestudy.service.domain.users.AppUser;
+import com.georgiimalyshev.storestudy.service.domain.users.Customer;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerRegistrationServiceTest {
+	private String email;
+	private String password;
+	boolean registrationSuccess;
+	
+	@Mock
+	private CustomerDao customerDao;
+	
+	@InjectMocks
+	private CustomerRegistrationService customerRegistrationService;
+	
+	private void setUpCustomerDaoMockToReturnOptionalOfAppUser(Optional<AppUser> optional) {
+		when(customerDao.findByEmail(email)).thenReturn(optional);
+	}
+	
+	@BeforeEach
+	public void registrationSuccessReset() {
+		registrationSuccess = false;
+	}
+	
+	@Test
+	public void givenNonOccupiedEmailAndNonEmptyPassword_whenRegisterNewCustomer_thenReturnTrue() {
+		email = "email1@mail.com";
+		password = "password1";
+		
+		registrationSuccess = customerRegistrationService.registerNewCustomer(email, password);
+		
+		assertTrue(registrationSuccess);
+	}
+	
+	@Test
+	public void givenOccupiedEmailAndNonEmptyPassword_whenRegisterNewCustomer_thenReturnFalse() {
+		email = "email2@mail.com";
+		password = "password1";
+		AppUser customer = new Customer();
+		customer.setEmail(email);
+		customer.setPassword(password);
+		setUpCustomerDaoMockToReturnOptionalOfAppUser(Optional.of(customer));
+		
+		registrationSuccess = customerRegistrationService.registerNewCustomer(email, password);
+		
+		assertFalse(registrationSuccess);
+	}
+	
+	@Test
+	public void givenNonOccupiedEmailAndEmptyPassword_whenRegisterNewCustomer_thenReturnFalse() {
+		email = "email1@mail.com";
+		password = "";
+		
+		registrationSuccess = customerRegistrationService.registerNewCustomer(email, password);
+		
+		assertFalse(registrationSuccess);
+	}
+}
