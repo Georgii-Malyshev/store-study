269960ad0db3f873eba996e6a264ea8439d760cb
Georgii-Malyshev <terminal451@gmail.com>
2021-07-27 10:33:34 +0500

    Add unit test for ProductManagementService

--- src/test/java/com/georgiimalyshev/storestudy/service/ProductManagementServiceTest.java
+++ src/test/java/com/georgiimalyshev/storestudy/service/ProductManagementServiceTest.java
@@ -19,55 +19,78 @@ import com.georgiimalyshev.storestudy.dao.ProductCatalogDao;
 import com.georgiimalyshev.storestudy.dao.ProductCategoryDao;
 import com.georgiimalyshev.storestudy.dao.ProductDao;
 import com.georgiimalyshev.storestudy.service.domain.store.Product;
+import com.georgiimalyshev.storestudy.service.domain.store.ProductCategory;
 import com.georgiimalyshev.storestudy.service.domain.users.AppUser;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductManagementServiceTest {
 	private int id;
 	private Product product1 = new Product();
-	
+	private ProductCategory productCategory1 = new ProductCategory();
+
 	@BeforeEach
 	public void productSetUp() {
-		id = 1;
 		product1.setId(1);
 		product1.setName("productName1");
 		product1.setPrice(10);
 	}
-	
+
+	@BeforeEach
+	public void productCategorySetUp() {
+		productCategory1.setId(1);
+		productCategory1.setName("productCategoryName1");
+	}
+
 	@Mock
 	private ProductCatalogDao productCatalogDao;
 	@Mock
 	private ProductCategoryDao productCategoryDao;
 	@Mock
 	private ProductDao productDao;
-	
+
 	@InjectMocks
 	private ProductManagementService productManagementService;
-	
+
 	private void setUpProductDaoMockToReturnOptionalOfProductDao(Optional<Product> optional) {
 		when(productDao.findById(id)).thenReturn(optional);
 	}
-	
+
+	private void setUpProductCategoryDaoMockToReturnOptionalOfProductCategoryDao(Optional<ProductCategory> optional) {
+		when(productCategoryDao.findById(id)).thenReturn(optional);
+	}
+
 	@Test
 	public void givenCorrectId_whenGetProductById_thenReturnCorrespondingProduct() {
 		id = 1;
-		
+
 		Optional<Product> optionalOfProduct1 = Optional.of(product1);
 		setUpProductDaoMockToReturnOptionalOfProductDao(optionalOfProduct1);
 		Product product = productManagementService.getProductById(id);
-		
+
 		// TODO use equals() instead
-		assertAll(() -> assertEquals(1, product.getId()),
-				() -> assertEquals("productName1", product.getName()),
+		assertAll(() -> assertEquals(1, product.getId()), () -> assertEquals("productName1", product.getName()),
 				() -> assertEquals(10, product.getPrice()));
 	}
-	
+
 	@Test
 	public void givenNonExistingId_whenGetProductById_thenThrowNoSuchElementException() {
 		id = 99;
-		
+
 		assertThrows(NoSuchElementException.class, () -> {
 			productManagementService.getProductById(id);
 		});
+	}
+
+	@Test
+	public void givenCorrectId_whenGetProductCategoryById_thenReturnCorrespondingProductCategory() {
+		id = 1;
+
+		Optional<ProductCategory> optionalOfProductCategory1 = Optional.of(productCategory1);
+		setUpProductCategoryDaoMockToReturnOptionalOfProductCategoryDao(optionalOfProductCategory1);
+		ProductCategory productCategory = productManagementService.getProductCategoryById(id);
+
+		// TODO use equals() instead
+		assertAll(() -> assertEquals(1, productCategory.getId()),
+				() -> assertEquals("productCategoryName1", productCategory.getName()));
 	}
 }
