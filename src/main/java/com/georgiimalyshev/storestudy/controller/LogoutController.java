e7c7904dfbbf4f35d4d9af7e82c2c8b116691397
Georgii-Malyshev <terminal451@gmail.com>
2021-08-04 17:20:47 +0500

    Delete cookies on the client when logging out

--- src/main/java/com/georgiimalyshev/storestudy/controller/LogoutController.java
+++ src/main/java/com/georgiimalyshev/storestudy/controller/LogoutController.java
@@ -1,6 +1,8 @@
 package com.georgiimalyshev.storestudy.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
@@ -12,11 +14,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LogoutController {
 	@GetMapping("/logout")
-	public String logout(HttpServletRequest httpServletRequest, Model model) {
+	public String logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Model model) {
 		HttpSession httpSession = httpServletRequest.getSession(false);
 		if (httpSession != null) {
 			httpSession.removeAttribute("user");
 			httpSession.invalidate();
+			// delete all cookies
+			for (Cookie cookie : httpServletRequest.getCookies()) {
+                String cookieName = cookie.getName();
+                Cookie cookieToDelete = new Cookie(cookieName, null);
+                cookieToDelete.setMaxAge(0);
+                httpServletResponse.addCookie(cookieToDelete);
+			}
 		} else {
 			Logger logger = LogManager.getLogger(LogoutController.class);
 			logger.warn("logout method called without a session");
