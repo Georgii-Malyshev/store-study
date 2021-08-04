8e40adf2a0f3f36f03ceb6c277094f11612de21c
Georgii-Malyshev <terminal451@gmail.com>
2021-08-04 11:45:02 +0500

    Add error handling to CartController
    Handle possible situations:
      - trying to access a shopping cart when not logged in
      - trying to access a shopping cart when logged in not as
        a Customer but as some other type of AppUser

--- src/main/java/com/georgiimalyshev/storestudy/controller/CartController.java
+++ src/main/java/com/georgiimalyshev/storestudy/controller/CartController.java
@@ -1,5 +1,7 @@
 package com.georgiimalyshev.storestudy.controller;
 
+import java.util.NoSuchElementException;
+
 import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
@@ -15,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.georgiimalyshev.storestudy.service.CartService;
 import com.georgiimalyshev.storestudy.service.ProductManagementService;
 import com.georgiimalyshev.storestudy.service.domain.store.Cart;
-import com.georgiimalyshev.storestudy.service.domain.store.CartItem;
 import com.georgiimalyshev.storestudy.service.domain.store.Product;
 import com.georgiimalyshev.storestudy.service.domain.users.Customer;
 
@@ -29,36 +30,43 @@ public class CartController {
 
 	private CartService cartService;
 	private ProductManagementService productManagementService;
+	private String NotLoggedInErrorMessage = "could not access shopping cart: no customer was found in session; "
+			+ "not logged in or session expired?";
+	private String NotACustomerErrorMessage = "could not access shopping cart: "
+			+ "logged in as some type of user other than customer; "
+			+ "log in using a customer account to be able to access a shopping cart";
 
 	@GetMapping("/shopping-cart")
 	public String cartPage(HttpSession httpSession, Model model) {
+		String resultString = "error";
 		Customer customer = (Customer) httpSession.getAttribute("user");
 		if (customer != null) {
 			Cart cart = customer.getCart();
 			int cartId = cart.getId();
 			cart = cartService.getCartByIdAndFetchCartItems(cartId);
 			model.addAttribute("cart", cart);
+			resultString = "cart";
 		} else {
-			model.addAttribute("errorMessage",
-					"could not display shopping cart because no customer was found in session "
-							+ "(not logged in or session expired?)");
-			return "error";
+			model.addAttribute("errorMessage", NotLoggedInErrorMessage);
 		}
-		return "cart";
+		return resultString;
 	}
 
 	@PostMapping("/add-to-cart")
 	public String addToCart(@RequestParam(name = "id") int productId, @RequestParam(name = "quantity") int quantity,
-			HttpSession httpSession) {
+			HttpSession httpSession, Model model) {
 		String resultString = "error";
-		// TODO cast will fail with exception if user is some other subtype of
-		// AppUserAbstract
-		Customer customer = (Customer) httpSession.getAttribute("user");
-		Cart cart = customer.getCart();
-		Product product = productManagementService.getProductById(productId);
-		cartService.fetchCartItemsAndAddProductToCart(cart, product, quantity);
-		resultString = "shopping-cart";
-		// TODO handle possible NoSuchElementException, ClassCastException etc.
-		return "redirect:" + resultString;
+		try {
+			Customer customer = (Customer) httpSession.getAttribute("user");
+			Cart cart = customer.getCart(); // every customer MUST ALWAYS have a corresponding cart
+			Product product = productManagementService.getProductById(productId);
+			cartService.fetchCartItemsAndAddProductToCart(cart, product, quantity);
+			resultString = "redirect:shopping-cart";
+		} catch (ClassCastException ex) {
+			model.addAttribute("errorMessage", NotACustomerErrorMessage);
+		} catch (NoSuchElementException ex) {
+			model.addAttribute("errorMessage", NotLoggedInErrorMessage);
+		}
+		return resultString;
 	}
 }
